ea323be4970482a715f9c927a8aec9712211e818
David Halewood <davidnhalewood@hotmail.com>
2017-06-14 21:46:49 +0100

    Create bot.rb
    Removed Access and Token Keys

--- bot.rb
+++ bot.rb
@@ -5,12 +5,10 @@ require 'twitter'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "2oGAPJw4SdmQnX6Z1LMNKeBTD"
-  config.consumer_secret     = "BGdkFOGwFP3x7FAxQhMcQOVT8D1f9i1rf1OWA5f1mf6O6kGAMN"
-  config.access_token        = "3657638057-qskcOAbfyBd5XZnKvgFpkIoVDGaHMA18GiGciBA"
-  config.access_token_secret = "4p6Lyl2B2nGmCMTMABNj6oh00IyTSPYwMSzX4mvTRyiTa"
+  config.consumer_key        = "XXXX"
+  config.consumer_secret     = "XXXX"
+  config.access_token        = "XXXX"
+  config.access_token_secret = "XXXX"
 end
 
-client.search("#ruby").take(50).each do |tweet|
-     puts tweet.text
-end
+client.update "My first tweet from the command line, neat huh?"
